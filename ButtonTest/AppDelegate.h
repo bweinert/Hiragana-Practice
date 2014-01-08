5e7081da483a61909cad4dceb110c51a6b3ab56b
Brittany Weinert <brittanyweinert@brittanys-mbp.gateway.2wire.net>
2014-01-07 21:24:56 -0600

    Initial Commit

---
+++ ButtonTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ButtonTest
+//
+//  Created by Brittany Weinert on 1/7/14.
+//  Copyright (c) 2014 Brittany Weinert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
