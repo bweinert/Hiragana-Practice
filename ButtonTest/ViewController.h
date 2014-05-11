0df15466634b30fb4af92367c9b944a4dfb12283
Brittany Weinert <bweinert@utexas.edu>
2014-05-10 21:38:52 -0500

    First Commit

--- ButtonTest/ViewController.h
+++ ButtonTest/ViewController.h
@@ -10,4 +10,69 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *currentCharacter;
+@property (strong, nonatomic) IBOutlet UILabel *statusText;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttona;
+@property (strong, nonatomic) IBOutlet UIButton *buttoni;
+@property (strong, nonatomic) IBOutlet UIButton *buttonu;
+@property (strong, nonatomic) IBOutlet UIButton *buttone;
+@property (strong, nonatomic) IBOutlet UIButton *buttono;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonka;
+@property (strong, nonatomic) IBOutlet UIButton *buttonki;
+@property (strong, nonatomic) IBOutlet UIButton *buttonku;
+@property (strong, nonatomic) IBOutlet UIButton *buttonke;
+@property (strong, nonatomic) IBOutlet UIButton *buttonko;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonta;
+@property (strong, nonatomic) IBOutlet UIButton *buttonchi;
+@property (strong, nonatomic) IBOutlet UIButton *buttontsu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonte;
+@property (strong, nonatomic) IBOutlet UIButton *buttonto;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonsa;
+@property (strong, nonatomic) IBOutlet UIButton *buttonshi;
+@property (strong, nonatomic) IBOutlet UIButton *buttonsu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonse;
+@property (strong, nonatomic) IBOutlet UIButton *buttonso;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonna;
+@property (strong, nonatomic) IBOutlet UIButton *buttonni;
+@property (strong, nonatomic) IBOutlet UIButton *buttonnu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonne;
+@property (strong, nonatomic) IBOutlet UIButton *buttonno;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonha;
+@property (strong, nonatomic) IBOutlet UIButton *buttonhi;
+@property (strong, nonatomic) IBOutlet UIButton *buttonfu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonhe;
+@property (strong, nonatomic) IBOutlet UIButton *buttonho;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonma;
+@property (strong, nonatomic) IBOutlet UIButton *buttonmi;
+@property (strong, nonatomic) IBOutlet UIButton *buttonmu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonme;
+@property (strong, nonatomic) IBOutlet UIButton *buttonmo;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonra;
+@property (strong, nonatomic) IBOutlet UIButton *buttonri;
+@property (strong, nonatomic) IBOutlet UIButton *buttonru;
+@property (strong, nonatomic) IBOutlet UIButton *buttonre;
+@property (strong, nonatomic) IBOutlet UIButton *buttonro;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonwa;
+@property (strong, nonatomic) IBOutlet UIButton *buttonwo;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonya;
+@property (strong, nonatomic) IBOutlet UIButton *buttonyu;
+@property (strong, nonatomic) IBOutlet UIButton *buttonyo;
+
+@property (strong, nonatomic) IBOutlet UIButton *buttonn;
+
+
+
+- (IBAction)buttonPressed:(UIButton *)sender;
+
 @end
+
--- ButtonTest/ViewController.m
+++ ButtonTest/ViewController.m
@@ -7,23 +7,139 @@
 //
 
 #import "ViewController.h"
+#include <stdlib.h>
 
-@interface ViewController ()
-
+@interface ViewController () {
+    NSDictionary *hiraganaTable;
+}
+@property (strong, nonatomic) NSDictionary *hiraganaTable;
 @end
 
 @implementation ViewController
+@synthesize hiraganaTable;
+@synthesize statusText;
+@synthesize currentCharacter;
 
-- (void)viewDidLoad
-{
+@synthesize buttoni;
+@synthesize buttona;
+@synthesize buttonu;
+@synthesize buttone;
+@synthesize buttono;
+
+@synthesize buttonka;
+@synthesize buttonki;
+@synthesize buttonku;
+@synthesize buttonke;
+@synthesize buttonko;
+
+@synthesize buttonta;
+@synthesize buttonchi;
+@synthesize buttontsu;
+@synthesize buttonte;
+@synthesize buttonto;
+
+@synthesize buttonsa;
+@synthesize buttonshi;
+@synthesize buttonsu;
+@synthesize buttonse;
+@synthesize buttonso;
+
+@synthesize buttonna;
+@synthesize buttonni;
+@synthesize buttonnu;
+@synthesize buttonne;
+@synthesize buttonno;
+
+@synthesize buttonha;
+@synthesize buttonhi;
+@synthesize buttonfu;
+@synthesize buttonhe;
+@synthesize buttonho;
+
+@synthesize buttonma;
+@synthesize buttonmi;
+@synthesize buttonmu;
+@synthesize buttonme;
+@synthesize buttonmo;
+
+@synthesize buttonra;
+@synthesize buttonri;
+@synthesize buttonru;
+@synthesize buttonre;
+@synthesize buttonro;
+
+@synthesize buttonwa;
+@synthesize buttonwo;
+
+@synthesize buttonya;
+@synthesize buttonyu;
+@synthesize buttonyo;
+
+@synthesize buttonn;
+
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Array of romanji (alphabetic readings of hiragana)
+    NSArray *alphabet = [NSArray arrayWithObjects: @"a", @"i", @"u", @"e", @"o",
+                         @"ka", @"ki", @"ku", @"ke", @"ko",
+                         @"ta", @"chi", @"tsu", @"te", @"to",
+                         @"sa", @"shi", @"su", @"se", @"so",
+                         @"na", @"ni", @"nu", @"ne", @"no",
+                         @"ha", @"hi", @"fu", @"he", @"ho",
+                         @"ma", @"mi", @"mu", @"me", @"mo",
+                         @"ra", @"ri", @"ru", @"re", @"ro",
+                         @"ya", @"wa", @"yu", @"wo", @"yo",
+                         @"n", nil];
+    
+    //Array of hiragana characters
+    NSArray *hiragana = [NSArray arrayWithObjects: @"あ", @"い", @"う", @"え", @"お",
+                         @"か", @"き", @"く", @"け", @"こ",
+                         @"た", @"ち", @"つ", @"て", @"と",
+                         @"さ", @"し", @"す", @"せ", @"そ",
+                         @"な", @"に", @"ぬ", @"ね", @"の",
+                         @"は", @"ひ", @"ふ", @"へ", @"ほ",
+                         @"ま", @"み", @"む", @"め", @"も",
+                         @"ら", @"り", @"る", @"れ", @"ろ",
+                         @"や", @"わ", @"ゆ", @"を", @"よ",
+                         @"ん", nil];
+    
+    //NSDictionary that maps each element in hiragana to alphabet
+    hiraganaTable = [NSDictionary dictionaryWithObjects: hiragana forKeys: alphabet];
 }
 
-- (void)didReceiveMemoryWarning
-{
+//method that picks the next character to guess
+- (void)setCharacter {
+    
+    NSArray *keys = [hiraganaTable allKeys];
+    NSUInteger index = arc4random() % [keys count];
+    
+    currentCharacter.text = [hiraganaTable objectForKey:[keys objectAtIndex:index]];
+}
+
+//IBAction method triggered whenever a hiragana character button is pressed
+//checks to see if it is correct or not and prints out a message. If it
+//is correct, a new character is chosen.
+- (IBAction)buttonPressed:(UIButton *)sender {
+    NSString *title = [sender titleForState:UIControlStateNormal];
+
+    
+    if ([currentCharacter.text isEqualToString: [hiraganaTable objectForKey: title]]) {
+        statusText.text = @"correct, new character changed";
+        [self setCharacter];
+        
+    }
+    else {
+        statusText.text = @"wrong";
+    }
+
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
